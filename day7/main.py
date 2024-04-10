3875e2ed5a6a2149557ee43ee68ff19ad1f2e04a
Meir <maornet@gmail.com>
2024-04-10 18:53:23 +0300

    refactor and part2

--- day7/main.py
+++ day7/main.py
@@ -4,8 +4,6 @@ import operator
 import queue
 from typing import Iterator
 
-import more_itertools
-
 
 class Op(enum.IntEnum):
     ADD = 1
@@ -39,135 +37,144 @@ def parse_input(input: str) -> list[int]:
     return list(map(int, input.strip().split(",")))
 
 
-def intcode_computer(opcodes: list[int], inputs: list[str]) -> Iterator[int]:
-    opcodes = opcodes[:]
-    inputs = inputs[:]
+class VM:
+    def __init__(self, opcodes: list[int]) -> None:
+        self._queue: queue.Queue[int] = queue.Queue()
+        self._opcodes = opcodes[:]
+        self._iter = iter(self)
 
-    i = 0
+    def __next__(self) -> int:
+        return next(self._iter)
 
-    is_immediate = lambda val: val == "1"
+    def __iter__(self) -> Iterator[int]:
+        opcodes = self._opcodes
 
-    while i < len(opcodes):
-        opcode = str(opcodes[i]).zfill(5)
+        i = 0
 
-        """
-        ABCDE
+        is_immediate = lambda val: val == "1"
 
-        DE - two-digit opcode,      02 == opcode 2
-        C - mode of 1st parameter,  0 == position mode
-        B - mode of 2nd parameter,  1 == immediate mode
-        A - mode of 3rd parameter,  0 == position mode,
-        """
+        while i < len(opcodes):
+            opcode = str(opcodes[i]).zfill(5)
 
-        instruction = Op(int(opcode[3:]))
+            """
+            ABCDE
 
-        if instruction in [Op.ADD, Op.MUL]:
-            c = opcodes[i + 1] if is_immediate(opcode[Parameter.C]) else opcodes[opcodes[i + 1]]
-            b = opcodes[i + 2] if is_immediate(opcode[Parameter.B]) else opcodes[opcodes[i + 2]]
+            DE - two-digit opcode,      02 == opcode 2
+            C - mode of 1st parameter,  0 == position mode
+            B - mode of 2nd parameter,  1 == immediate mode
+            A - mode of 3rd parameter,  0 == position mode,
+            """
 
-            opcodes[opcodes[i + 3]] = OP_FN[instruction](c, b)
+            instruction = Op(int(opcode[3:]))
 
-            i += 4
-        elif instruction == Op.INPUT:
-            opcodes[opcodes[i + 1]] = inputs.pop(0)
+            if instruction in [Op.ADD, Op.MUL]:
+                c = (
+                    opcodes[i + 1]
+                    if is_immediate(opcode[Parameter.C])
+                    else opcodes[opcodes[i + 1]]
+                )
+                b = (
+                    opcodes[i + 2]
+                    if is_immediate(opcode[Parameter.B])
+                    else opcodes[opcodes[i + 2]]
+                )
 
-            i += 2
-        elif instruction == Op.OUTPUT:
-            c = opcodes[i + 1] if is_immediate(opcode[Parameter.C]) else opcodes[opcodes[i + 1]]
+                opcodes[opcodes[i + 3]] = OP_FN[instruction](c, b)
 
-            yield c
+                i += 4
+            elif instruction == Op.INPUT:
+                opcodes[opcodes[i + 1]] = self._queue.get()
 
-            i += 2
-        elif instruction in [Op.JUMP_IF_TRUE, Op.JUMP_IF_FALSE]:
-            c = opcodes[i + 1] if is_immediate(opcode[Parameter.C]) else opcodes[opcodes[i + 1]]
-            b = opcodes[i + 2] if is_immediate(opcode[Parameter.B]) else opcodes[opcodes[i + 2]]
+                i += 2
+            elif instruction == Op.OUTPUT:
+                c = (
+                    opcodes[i + 1]
+                    if is_immediate(opcode[Parameter.C])
+                    else opcodes[opcodes[i + 1]]
+                )
 
-            i = b if OP_FN[instruction](c, 0) else i + 3
-        elif instruction in [Op.LESS_THEN, Op.EQUAL]:
-            c = opcodes[i + 1] if is_immediate(opcode[Parameter.C]) else opcodes[opcodes[i + 1]]
-            b = opcodes[i + 2] if is_immediate(opcode[Parameter.B]) else opcodes[opcodes[i + 2]]
+                yield c
 
-            opcodes[opcodes[i + 3]] = OP_FN[instruction](c, b)
+                i += 2
+            elif instruction in [Op.JUMP_IF_TRUE, Op.JUMP_IF_FALSE]:
+                c = (
+                    opcodes[i + 1]
+                    if is_immediate(opcode[Parameter.C])
+                    else opcodes[opcodes[i + 1]]
+                )
+                b = (
+                    opcodes[i + 2]
+                    if is_immediate(opcode[Parameter.B])
+                    else opcodes[opcodes[i + 2]]
+                )
 
-            i += 4
-        elif instruction == Op.HALT:
-            break
+                i = b if OP_FN[instruction](c, 0) else i + 3
+            elif instruction in [Op.LESS_THEN, Op.EQUAL]:
+                c = (
+                    opcodes[i + 1]
+                    if is_immediate(opcode[Parameter.C])
+                    else opcodes[opcodes[i + 1]]
+                )
+                b = (
+                    opcodes[i + 2]
+                    if is_immediate(opcode[Parameter.B])
+                    else opcodes[opcodes[i + 2]]
+                )
 
+                opcodes[opcodes[i + 3]] = OP_FN[instruction](c, b)
 
-def intcode_computer2(opcodes: list[int], inputs: queue.Queue) -> Iterator[int]:
-    opcodes = opcodes[:]
+                i += 4
+            elif instruction == Op.HALT:
+                break
 
-    i = 0
-
-    is_immediate = lambda val: val == "1"
-
-    while i < len(opcodes):
-        opcode = str(opcodes[i]).zfill(5)
-
-        """
-        ABCDE
-
-        DE - two-digit opcode,      02 == opcode 2
-        C - mode of 1st parameter,  0 == position mode
-        B - mode of 2nd parameter,  1 == immediate mode
-        A - mode of 3rd parameter,  0 == position mode,
-        """
-
-        instruction = Op(int(opcode[3:]))
-
-        if instruction in [Op.ADD, Op.MUL]:
-            c = opcodes[i + 1] if is_immediate(opcode[Parameter.C]) else opcodes[opcodes[i + 1]]
-            b = opcodes[i + 2] if is_immediate(opcode[Parameter.B]) else opcodes[opcodes[i + 2]]
-
-            opcodes[opcodes[i + 3]] = OP_FN[instruction](c, b)
-
-            i += 4
-        elif instruction == Op.INPUT:
-            opcodes[opcodes[i + 1]] = inputs.get()
-
-            i += 2
-        elif instruction == Op.OUTPUT:
-            c = opcodes[i + 1] if is_immediate(opcode[Parameter.C]) else opcodes[opcodes[i + 1]]
-
-            yield c
-
-            i += 2
-        elif instruction in [Op.JUMP_IF_TRUE, Op.JUMP_IF_FALSE]:
-            c = opcodes[i + 1] if is_immediate(opcode[Parameter.C]) else opcodes[opcodes[i + 1]]
-            b = opcodes[i + 2] if is_immediate(opcode[Parameter.B]) else opcodes[opcodes[i + 2]]
-
-            i = b if OP_FN[instruction](c, 0) else i + 3
-        elif instruction in [Op.LESS_THEN, Op.EQUAL]:
-            c = opcodes[i + 1] if is_immediate(opcode[Parameter.C]) else opcodes[opcodes[i + 1]]
-            b = opcodes[i + 2] if is_immediate(opcode[Parameter.B]) else opcodes[opcodes[i + 2]]
-
-            opcodes[opcodes[i + 3]] = OP_FN[instruction](c, b)
-
-            i += 4
-        elif instruction == Op.HALT:
-            break
+    def send(self, value: int) -> None:
+        self._queue.put(value)
 
 
-def run(input: str, in_: list[int]) -> int:
+def solution(input: str, part: int) -> int:
     opcodes = parse_input(input)
 
-    return more_itertools.last(intcode_computer(opcodes, in_))
+    if part == 1:
+        settings = range(5)
+    else:
+        settings = range(5, 10)
 
-
-def part1(input: str) -> int:
     max_signal = 0
 
-    for setting in itertools.permutations(range(5)):
+    for setting in itertools.permutations(settings):
+        vms: list[VM] = []
+
         second_input = 0
+
         for i in setting:
-            second_input = run(input, [i, second_input])
-        max_signal = max(max_signal, second_input)
+            vm = VM(opcodes)
+            vm.send(i)
+            vm.send(second_input)
+            vms.append(vm)
+
+            second_input = next(vm)
+
+        if part == 1:
+            max_signal = max(max_signal, second_input)
+        else:
+            for vm in itertools.cycle(vms):
+                try:
+                    vm.send(second_input)
+                    second_input = next(vm)
+                except StopIteration:
+                    if vm is vms[-1]:
+                        max_signal = max(max_signal, second_input)
+                        break
 
     return max_signal
 
 
+def part1(input: str) -> int:
+    return solution(input, 1)
+
+
 def part2(input: str) -> int:
-    pass
+    return solution(input, 2)
 
 
 def main() -> None:
