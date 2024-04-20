326b9d87be98c18f75d9d22a854a8f0fc27ffc0d
Meir <maornet@gmail.com>
2024-04-20 21:26:33 +0300

    Day17 - Part1 (#19)

---
+++ day17/test_main.py
@@ -0,0 +1,9 @@
+from .main import part1, part2
+
+
+def test_input():
+    with open("input.txt") as file:
+        input = file.read()
+
+    assert part1(input) == 8928
+    assert part2(input) == 880360
