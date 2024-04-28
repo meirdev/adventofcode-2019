f7514505945c793084de8f90004b777975ca5dcd
Meir <maornet@gmail.com>
2024-04-28 12:21:32 +0300

    day25

---
+++ day25/main.py
@@ -0,0 +1,152 @@
+import builtins
+import collections
+import enum
+import operator
+import queue
+from typing import Iterator
+
+import threading
+
+
+class Op(enum.IntEnum):
+    ADD = 1
+    MUL = 2
+    INPUT = 3
+    OUTPUT = 4
+    JUMP_IF_TRUE = 5
+    JUMP_IF_FALSE = 6
+    LESS_THEN = 7
+    EQUAL = 8
+    ADJUST = 9
+    HALT = 99
+
+
+class Parameter(enum.IntEnum):
+    A = 0
+    B = 1
+    C = 2
+
+
+class ParameterMode(enum.StrEnum):
+    POSITION = "0"
+    IMMEDIATE = "1"
+    RELATIVE = "2"
+
+
+OP_FN = {
+    Op.ADD: operator.add,
+    Op.MUL: operator.mul,
+    Op.JUMP_IF_TRUE: operator.ne,
+    Op.JUMP_IF_FALSE: operator.eq,
+    Op.LESS_THEN: operator.lt,
+    Op.EQUAL: operator.eq,
+}
+
+
+def parse_input(input: str) -> list[int]:
+    return list(map(int, input.strip().split(",")))
+
+
+class VM:
+    def __init__(self, opcodes: list[int]) -> None:
+        self._queue: queue.Queue[int] = queue.Queue()
+
+        memory = collections.defaultdict(int)
+
+        for i, val in enumerate(opcodes):
+            memory[i] = val
+
+        self._memory = memory
+        self._relative_base = 0
+        self._iter = iter(self)
+
+    def __next__(self) -> int:
+        return next(self._iter)
+
+    def __iter__(self) -> Iterator[int]:
+        memory = self._memory
+        relative_base = self._relative_base
+
+        i = 0
+
+        while i < len(memory):
+            opcode = str(memory[i]).zfill(5)
+
+            instruction = Op(int(opcode[3:]))
+
+            parameters = []
+
+            for param, rel in zip((Parameter.C, Parameter.B, Parameter.A), (1, 2, 3)):
+                if opcode[param] == ParameterMode.IMMEDIATE:
+                    parameters.append(i + rel)
+                elif opcode[param] == ParameterMode.RELATIVE:
+                    parameters.append(relative_base + memory[i + rel])
+                elif opcode[param] == ParameterMode.POSITION:
+                    parameters.append(memory[i + rel])
+
+            if instruction in [Op.ADD, Op.MUL]:
+                memory[parameters[2]] = OP_FN[instruction](
+                    memory[parameters[0]], memory[parameters[1]]
+                )
+
+                i += 4
+            elif instruction == Op.INPUT:
+                memory[parameters[0]] = self._queue.get()
+
+                i += 2
+            elif instruction == Op.OUTPUT:
+                yield memory[parameters[0]]
+
+                i += 2
+            elif instruction in [Op.JUMP_IF_TRUE, Op.JUMP_IF_FALSE]:
+                i = (
+                    memory[parameters[1]]
+                    if OP_FN[instruction](memory[parameters[0]], 0)
+                    else i + 3
+                )
+            elif instruction in [Op.LESS_THEN, Op.EQUAL]:
+                memory[parameters[2]] = int(
+                    OP_FN[instruction](memory[parameters[0]], memory[parameters[1]])
+                )
+
+                i += 4
+            elif instruction == Op.ADJUST:
+                relative_base += memory[parameters[0]]
+
+                i += 2
+            elif instruction == Op.HALT:
+                break
+
+    def send(self, value: int) -> None:
+        self._queue.put(value)
+
+
+def part1(input: str) -> int:
+    opcodes = parse_input(input)
+
+    vm = VM(opcodes)
+
+    def get_input():
+        while True:
+            command = builtins.input().strip()
+
+            for i in command:
+                vm.send(ord(i))
+            vm.send(10)
+
+    thread = threading.Thread(target=get_input)
+    thread.start()
+
+    for i in vm:
+        print(chr(i), end="")
+
+
+def main() -> None:
+    with open("input.txt") as file:
+        input = file.read()
+
+    print("Part 1:", part1(input))
+
+
+if __name__ == "__main__":
+    main()
---
+++ day25/test_main.py
@@ -0,0 +1,8 @@
+from .main import part1
+
+
+def test_input():
+    with open("input.txt") as file:
+        input = file.read()
+
+    assert part1(input) == 278664
